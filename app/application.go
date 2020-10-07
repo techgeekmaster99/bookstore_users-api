818c3c8dbe8dcfbe34cff094974c3c253032a444
Anand K <anand.nk87@gmail.com>
2020-10-07 23:43:07 +0530

    version_1

---
+++ app/application.go
@@ -0,0 +1,12 @@
+package app
+
+
+import (
+           "github.com/gin-gonic/gin"
+)
+
+func StartApplication(){
+
+
+
+}
---
+++ mian.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/techgeekmaster99/bookstore_users-api/app"
+	"fmt"
+)
+
+func main(){
+
+  fmt.Println("main Starts ....")
+  app.StartApplication()
+
+}
